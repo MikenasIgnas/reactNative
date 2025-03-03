25691846414f294b00eb3bd0fa62264bdf55f502
Ignas Mikenas <mikenasignas@gmail.com>
2025-03-03 20:23:38 +0200

    create basic my app structure

---
+++ app/(tabs)/contact.tsx
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function TabTwoScreen() {
+  return (
+    <View style={styles.container}><Text  style={styles.title}>Hello</Text></View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  title: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    marginBottom: 120,
+  },
+})
